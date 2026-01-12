9df1a6d1ce82ba4bab3a0db680f3232d3ee80fac
Hadream2730 <michaee455618@gmail.com>
2026-01-12 13:03:05 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,214 @@
+$(function () {
+    var gpt_4_1_item = null;
+    var gpt_5_1_item = null;
+    $('#problem-number').val('1523');
+
+    function displayAll() {
+        const problem_number = $('#problem-number').val();
+        console.log('Problem Number:', problem_number);
+        gpt_4_1_item = get_gpt_4_1_from_index(problem_number);
+        gpt_5_1_item = get_gpt_5_1_from_index(problem_number);
+        console.log('GPT-4.1 Item:', gpt_4_1_item);
+        console.log('GPT-5.1 Item:', gpt_5_1_item);
+        display_problem();
+        display_solution();
+        display_answer();
+    }
+
+  $('#display-button').click(function () {
+    displayAll();
+  });
+
+  $('#problem-number').on('keypress', function (e) {
+      if (e.which === 13) { // Enter key pressed
+          displayAll();
+      }
+  });
+  function get_gpt_4_1_from_index(index) {
+    for(let i=0; i < gpt_4_1_data.length; i++) {
+        if(gpt_4_1_data[i].index == index) {
+            return gpt_4_1_data[i]
+        }
+    }
+  }
+  function get_gpt_5_1_from_index(index) {
+    for(let i=0; i < gpt_5_1_data.length; i++) {
+        if(gpt_5_1_data[i].index == index) {
+            return gpt_5_1_data[i]
+        }
+    }
+  }
+  function display_problem() {
+    let outputDiv = document.getElementById('problem-display');
+    outputDiv.innerHTML = gpt_4_1_item.problem;
+    
+    // Add the text to be rendered
+    // const textNode = document.createTextNode(text);
+    // outputDiv.appendChild(textNode);
+    
+    // Render the KaTeX
+    try {
+        renderMathInElement(outputDiv, {
+            delimiters: [
+                {left: "$$", right: "$$", display: true},
+                {left: "$", right: "$", display: false},
+                {left: "\\(", right: "\\)", display: false},
+                {left: "\\[", right: "\\]", display: true},
+                {left: "\\begin{equation}", right: "\\end{equation}", display: true},
+                {left: "\\begin{align}", right: "\\end{align}", display: true},
+                {left: "\\begin{alignat}", right: "\\end{alignat}", display: true},
+                {left: "\\begin{gather}", right: "\\end{gather}", display: true},
+                {left: "\\begin{CD}", right: "\\end{CD}", display: true}
+            ],
+            throwOnError: false,
+            strict: false
+        });
+    } catch (error) {
+        outputDiv.innerHTML = '<div style="color: red;">Error rendering KaTeX: ' + error.message + '</div>';
+    }
+  }
+
+  
+  function display_solution() {
+    let outputDiv = document.getElementById('solution-display');
+    outputDiv.innerHTML = gpt_4_1_item.solution;
+    
+    // Add the text to be rendered
+    // const textNode = document.createTextNode(text);
+    // outputDiv.appendChild(textNode);
+    
+    // Render the KaTeX
+    try {
+        renderMathInElement(outputDiv, {
+            delimiters: [
+                {left: "$$", right: "$$", display: true},
+                {left: "$", right: "$", display: false},
+                {left: "\\(", right: "\\)", display: false},
+                {left: "\\[", right: "\\]", display: true},
+                {left: "\\begin{equation}", right: "\\end{equation}", display: true},
+                {left: "\\begin{align}", right: "\\end{align}", display: true},
+                {left: "\\begin{alignat}", right: "\\end{alignat}", display: true},
+                {left: "\\begin{gather}", right: "\\end{gather}", display: true},
+                {left: "\\begin{CD}", right: "\\end{CD}", display: true}
+            ],
+            throwOnError: false,
+            strict: false
+        });
+    } catch (error) {
+        outputDiv.innerHTML = '<div style="color: red;">Error rendering KaTeX: ' + error.message + '</div>';
+    }
+  }
+
+  
+  function display_answer() {
+    let outputDiv = document.getElementById('answer-display');
+    outputDiv.innerHTML = "$" + gpt_4_1_item.answers + "$";
+    
+    // Add the text to be rendered
+    // const textNode = document.createTextNode(text);
+    // outputDiv.appendChild(textNode);
+    
+    // Render the KaTeX
+    try {
+        renderMathInElement(outputDiv, {
+            delimiters: [
+                {left: "$$", right: "$$", display: true},
+                {left: "$", right: "$", display: false},
+                {left: "\\(", right: "\\)", display: false},
+                {left: "\\[", right: "\\]", display: true},
+                {left: "\\begin{equation}", right: "\\end{equation}", display: true},
+                {left: "\\begin{align}", right: "\\end{align}", display: true},
+                {left: "\\begin{alignat}", right: "\\end{alignat}", display: true},
+                {left: "\\begin{gather}", right: "\\end{gather}", display: true},
+                {left: "\\begin{CD}", right: "\\end{CD}", display: true}
+            ],
+            throwOnError: false,
+            strict: false
+        });
+    } catch (error) {
+        outputDiv.innerHTML = '<div style="color: red;">Error rendering KaTeX: ' + error.message + '</div>';
+    }
+  }
+
+  
+  function display_result_output(text) {
+    let outputDiv = document.getElementById('result-display');
+    outputDiv.innerHTML = text;
+    
+    // Add the text to be rendered
+    // const textNode = document.createTextNode(text);
+    // outputDiv.appendChild(textNode);
+    
+    // Render the KaTeX
+    try {
+        renderMathInElement(outputDiv, {
+            delimiters: [
+                {left: "$$", right: "$$", display: true},
+                {left: "$", right: "$", display: false},
+                {left: "\\(", right: "\\)", display: false},
+                {left: "\\[", right: "\\]", display: true},
+                {left: "\\begin{equation}", right: "\\end{equation}", display: true},
+                {left: "\\begin{align}", right: "\\end{align}", display: true},
+                {left: "\\begin{alignat}", right: "\\end{alignat}", display: true},
+                {left: "\\begin{gather}", right: "\\end{gather}", display: true},
+                {left: "\\begin{CD}", right: "\\end{CD}", display: true}
+            ],
+            throwOnError: false,
+            strict: false
+        });
+    } catch (error) {
+        outputDiv.innerHTML = '<div style="color: red;">Error rendering KaTeX: ' + error.message + '</div>';
+    }
+  }
+  
+  function display_model2_output() {
+    let outputDiv = document.getElementById('model2-display');
+    outputDiv.innerHTML = gpt_5_1_item.completion;
+    
+    // Add the text to be rendered
+    // const textNode = document.createTextNode(text);
+    // outputDiv.appendChild(textNode);
+    
+    // Render the KaTeX
+    try {
+        renderMathInElement(outputDiv, {
+            delimiters: [
+                {left: "$$", right: "$$", display: true},
+                {left: "$", right: "$", display: false},
+                {left: "\\(", right: "\\)", display: false},
+                {left: "\\[", right: "\\]", display: true},
+                {left: "\\begin{equation}", right: "\\end{equation}", display: true},
+                {left: "\\begin{align}", right: "\\end{align}", display: true},
+                {left: "\\begin{alignat}", right: "\\end{alignat}", display: true},
+                {left: "\\begin{gather}", right: "\\end{gather}", display: true},
+                {left: "\\begin{CD}", right: "\\end{CD}", display: true}
+            ],
+            throwOnError: false,
+            strict: false
+        });
+    } catch (error) {
+        outputDiv.innerHTML = '<div style="color: red;">Error rendering KaTeX: ' + error.message + '</div>';
+    }
+  }
+
+  $('#submit-button').click((async function () {
+        console.log('Submit is clicked')
+        await fetch("https://gpt-test-backend.onrender.com/chat", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                system: $('#system-instruction').val(),
+                messages: [
+                { role: "user", content: gpt_4_1_item.problem }
+                ]
+            })
+        })
+        .then(res => res.json())
+        .then(data => {
+            let result = data.content[0].content[0].text;
+            display_result_output(result)
+        });
+    }))
+});
