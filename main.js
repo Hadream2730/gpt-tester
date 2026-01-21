6d65a9cd96e0bde75f51752bba19edfe59e2db30
Hadream2730 <michaee455618@gmail.com>
2026-01-21 08:07:51 -0500

    Update

--- main.js
+++ main.js
@@ -16,14 +16,52 @@ $(function () {
     }
 
   $('#display-button').click(function () {
-    displayAll();
+      displayAll();
   });
-
+  $('#prev-button').click(function () {
+      showPrevious();
+  });
+  $('#next-button').click(function () {
+      showNext();
+  });
   $('#problem-number').on('keypress', function (e) {
       if (e.which === 13) { // Enter key pressed
           displayAll();
       }
   });
+  function showNext() {
+    const problem_number = $('#problem-number').val();
+    let next_problem_number = 0;
+    for(let i=0; i < gpt_4_1_data.length; i++) {
+        if(gpt_4_1_data[i].index == problem_number) {
+            if(i == gpt_4_1_data.length - 1) {
+                next_problem_number = gpt_4_1_data[0].index;
+                break;
+            }
+            next_problem_number =  gpt_4_1_data[i+1].index;
+            break;
+        }
+    }
+    $('#problem-number').val(next_problem_number);
+    displayAll(next_problem_number);
+  }
+  function showPrevious() {
+    const problem_number = $('#problem-number').val();
+    let previous_problem_number = 0;
+    for(let i=0; i < gpt_4_1_data.length; i++) {
+        if(gpt_4_1_data[i].index == problem_number) {
+            if(i == 0) {
+                previous_problem_number = gpt_4_1_data[gpt_4_1_data.length - 1].index;
+                break;
+            }
+            previous_problem_number =  gpt_4_1_data[i-1].index;
+            break;
+        }
+    }
+    $('#problem-number').val(previous_problem_number);
+    displayAll(previous_problem_number);
+}
+
   function get_gpt_4_1_from_index(index) {
     for(let i=0; i < gpt_4_1_data.length; i++) {
         if(gpt_4_1_data[i].index == index) {
